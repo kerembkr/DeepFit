89987edb5a225fce788d347140318622d54e2eab
Kerem Bükrü <kerembuekrue@gmail.com>
2024-05-23 10:45:00 +0200

    updated OOP DNN tensorflow file

--- src/DNN_tf_oop.py
+++ src/DNN_tf_oop.py
@@ -30,37 +30,107 @@ class DNN(keras.Model):
         super(DNN, self).compile(optimizer=optimizer, loss=loss, metrics=metrics, **kwargs)
 
     def fit(self, x, y, batch_size=None, epochs=1, verbose=1, validation_split=0.0, **kwargs):
-        return super(DNN, self).fit(x, y, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_split=validation_split, **kwargs)
+        return super(DNN, self).fit(x, y, batch_size=batch_size, epochs=epochs, verbose=verbose,
+                                    validation_split=validation_split, **kwargs)
+
+    # x = None,
+    # y = None,
+    # batch_size = None,
+    # epochs = 1,
+    # verbose = 'auto',
+    # callbacks = None,
+    # validation_split = 0.0,
+    # validation_data = None,
+    # shuffle = True,
+    # class_weight = None,
+    # sample_weight = None,
+    # initial_epoch = 0,
+    # steps_per_epoch = None,
+    # validation_steps = None,
+    # validation_batch_size = None,
+    # validation_freq = 1
 
     def predict(self, x, batch_size=None, verbose=0, steps=None, **kwargs):
         return super(DNN, self).predict(x, batch_size=batch_size, verbose=verbose, steps=steps, **kwargs)
 
 
-if __name__ == "__main__":
-    model = DNN()
+def show_compare(it):
+    test_df['pred'] = model.predict(test_df['X'])
+    plt.figure(figsize=(10, 6))
+    ax = plt.subplot()
+    ax.scatter(train_df['X'], train_df['y'], color="red")
+    ax.scatter(test_df['X'], test_df['pred'], color="purple")
+    ax.set_xlabel("X")
+    ax.set_ylabel("y")
+    ax.set_xlim([-1, 11])
+    ax.set_ylim([-0.1, 2.1])
+    ax.tick_params(direction="in", length=10, width=0.8, colors='black')
+    ax.spines['top'].set_linewidth(2.0)
+    ax.spines['bottom'].set_linewidth(2.0)
+    ax.spines['left'].set_linewidth(2.0)
+    ax.spines['right'].set_linewidth(2.0)
+    plt.savefig('../output/img_' + str(it) + '.png')
+    plt.close()
 
-    # Dummy data for demonstration
-    x_train = np.random.rand(100, 10)
-    y_train = np.random.rand(100, 1)
+
+def create_gif_and_cleanup(output_path, duration=500):
+    # Get list of all image files in the output directory
+    png_files = [f for f in os.listdir('../output') if f.endswith('.png')]
+    png_files.sort(key=lambda x: int(x.split('_')[1].split('.')[0]))  # Sort files numerically
+
+    # List to hold the images
+    images = []
+
+    # Open each file and add it to the images list
+    for file_name in png_files:
+        file_path = os.path.join('../output', file_name)
+        images.append(Image.open(file_path))
+
+    # Save images as a GIF
+    if images:
+        images[0].save(output_path, save_all=True, append_images=images[1:], optimize=False, duration=duration, loop=0)
+
+    # Delete the PNG files
+    for file_name in png_files:
+        file_path = os.path.join('../output', file_name)
+        os.remove(file_path)
+
+
+if __name__ == "__main__":
+
+    # training data
+    train_df = pd.DataFrame(np.random.sample([100000, 1]) * 10, columns=['X'])
+    train_df['y'] = train_df['X'].apply(lambda x: math.sin(x) + 1)
+    train_df = train_df.sample(len(train_df))
+
+    # test data
+    test_df = pd.DataFrame(np.random.sample([100, 1]) * 10, columns=['X'])
+
+    # plot training data
+    plt.scatter(train_df['X'], train_df['y'])
+
+    model = DNN()
 
     # Compile the model
     model.compile(
-        optimizer='rmsprop',
+        optimizer=keras.optimizers.Adam(learning_rate=0.00001),
         loss='mean_squared_error',
         metrics=['mean_absolute_error']
     )
 
     # Fit the model
-    model.fit(
-        x=x_train,
-        y=y_train,
-        batch_size=10,
-        epochs=5,
-        verbose=1,
-        validation_split=0.2
-    )
+    for i in range(100):
+        model.fit(
+            x=train_df['X'],
+            y=train_df['y'],
+            # batch_size=10,
+            epochs=1,
+            # verbose=1,
+            validation_split=0.2
+        )
 
-    # Predict with the model
-    x_test = np.random.rand(10, 10)
-    predictions = model.predict(x_test)
-    print(predictions)
+        show_compare(i)
+
+    # create animation and delete png-files
+    create_gif_and_cleanup('../output/animation.gif', duration=50)
+
